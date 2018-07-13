d72fd6bcb246d6b53ebe26c6cc31e002789785d7
Romel Devops <romelgudes.is.online@gmail.com>
2018-07-13 15:34:26 +0800

    update function child theme

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 function my_theme_enqueue_styles() {
 
-    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the GeneratePress Theme.
+    $parent_style = 'parent-style'; // This is 'generatepress-style' for the GeneratePress Theme
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
