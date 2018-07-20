7e9251df02226b24fdc0c2403451de6649e1d557
micah021297 <40780287+micah021297@users.noreply.github.com>
2018-07-20 10:16:48 +0800

    FOOTER PAGE
    I add javascript for smooth scroll functionality for coffee beans page

---
+++ footer.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package GeneratePress
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+
+	</div><!-- #content -->
+</div><!-- #page -->
+
+<?php
+/**
+ * generate_before_footer hook.
+ *
+ * @since 0.1
+ */
+do_action( 'generate_before_footer' );
+?>
+
+<div <?php generate_footer_class(); ?>>
+	<?php
+	/**
+	 * generate_before_footer_content hook.
+	 *
+	 * @since 0.1
+	 */
+	do_action( 'generate_before_footer_content' );
+
+	/**
+	 * generate_footer hook.
+	 *
+	 * @since 1.3.42
+	 *
+	 * @hooked generate_construct_footer_widgets - 5
+	 * @hooked generate_construct_footer - 10
+	 */
+	do_action( 'generate_footer' );
+
+	/**
+	 * generate_after_footer_content hook.
+	 *
+	 * @since 0.1
+	 */
+	do_action( 'generate_after_footer_content' );
+	?>
+</div><!-- .site-footer -->
+
+<?php
+/**
+ * generate_after_footer hook.
+ *
+ * @since 2.1
+ */
+do_action( 'generate_after_footer' );
+
+wp_footer();
+?>
+<script>
+    jQuery('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+            || location.hostname == this.hostname) {
+
+            var target = jQuery(this.hash);
+            target = target.length ? target : jQuery('[name=' + this.hash.slice(1) +']');
+               if (target.length) {
+                 jQuery('html,body').animate({
+                     scrollTop: target.offset().top
+                }, 900);
+                return false;
+            }
+        }
+    });
+</script>
+</body>
+</html>
